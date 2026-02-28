e8ecd06580169d849bc6abd9ade9c80529dca9be
Vishnu Surla <vishnuteja.surla@gmail.com>
2026-02-28 11:01:40 +0000

    Add and Comment out Non-Atomic Implementation for TSAN testing

--- Assignment-02/Snapshot.ml
+++ Assignment-02/Snapshot.ml
@@ -6,19 +6,37 @@ type 'a t = {
   n : int;                         (* Number of registers *)
 }
 
+(* type 'a t = {
+  registers : 'a array;  (* Array of registers *)
+  n : int;                         (* Number of registers *)
+} *)
+
 let create n init_value = 
   if n <= 0 then invalid_arg "The size of the array n must be greater than 0";
   let regs = Array.init n (fun _ -> Atomic.make init_value) in
   {registers = regs; n}
 
+(* let create n init_value = 
+  if n <= 0 then invalid_arg "The size of the array n must be greater than 0";
+  let regs = Array.make n init_value in
+  {registers = regs; n} *)
+
 let update snapshot idx value = 
   if idx < 0 || idx >= snapshot.n then invalid_arg "Index(idx) out of bounds";
   Atomic.set snapshot.registers.(idx) value
+
+(* let update snapshot idx value = 
+  if idx < 0 || idx >= snapshot.n then invalid_arg "Index(idx) out of bounds";
+  snapshot.registers.(idx) <- value *)
 
 (** Helper: collect all register values *)
 let collect snapshot =
   let c = Array.init snapshot.n (fun i -> Atomic.get snapshot.registers.(i)) in
   c
+
+(* let collect snapshot =
+  let c = Array.init snapshot.n (fun i -> snapshot.registers.(i)) in
+  c *)
 
 (** Scan using double-collect algorithm *)
 let scan snapshot =
